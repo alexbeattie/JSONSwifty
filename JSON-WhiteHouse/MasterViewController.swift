b1262452aae8060c4debebcfd78e26bb141e4436
Alex Beattie <alexanderbeattie@mac.com>
2015-11-04 07:24:23 -0800

    Added GCD

--- JSON-WhiteHouse/MasterViewController.swift
+++ JSON-WhiteHouse/MasterViewController.swift
@@ -10,17 +10,17 @@ import UIKit
 import SwiftyJSON
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
     var objects = [[String: String]]()
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         //////////////////////// begin 200 Status check ///////////////////////
         // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-       
+        
         let urlString: String
         
         if navigationController?.tabBarItem.tag == 0 {
@@ -29,30 +29,30 @@ class MasterViewController: UITableViewController {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
         
-        
-        if let url = NSURL(string: urlString) {
-            if let data = try? NSData(contentsOfURL: url, options:[]) {
-                let json = JSON(data: data)
-                
-                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
-                    // we're OK to parse
-
-                    parseJSON(json)
-                    print(json)
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) { [unowned self] in
+            
+            if let url = NSURL(string: urlString) {
+                if let data = try? NSData(contentsOfURL: url, options:[]) {
+                    let json = JSON(data: data)
                     
+                    if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                        // we're OK to parse
+                        
+                        self.parseJSON(json)
+                        print(json)
+                        
+                    } else {
+                        self.showError()
+                    }
                 } else {
-                    showError()
+                    self.showError()
                 }
             } else {
-                showError()
+                self.showError()
             }
-        } else {
-            showError()
+            /////////////////////// end of check OK:200 /////////////////////////
         }
-        /////////////////////// end of check OK:200 /////////////////////////
     }
-
-    
     
     //// parse the JSON /////
     func parseJSON(json: JSON) {
@@ -63,30 +63,33 @@ class MasterViewController: UITableViewController {
             let obj = ["title":title, "body":body, "sigs":sigs]
             objects.append(obj)
         }
-        tableView.reloadData()
+        dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+            self.tableView.reloadData()
+        }
     }
     
     
     func showError() {
-        let ac = UIAlertController(title: "Loading Error", message: "There was an error loading the feed; Please check your connection and try again.", preferredStyle: .Alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-        presentViewController(ac, animated: true, completion: nil)
-        
+        dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(ac, animated: true, completion: nil)
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     // MARK: - Segues
-
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
@@ -98,26 +101,26 @@ class MasterViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
+        
         let object = objects[indexPath.row]
         cell.textLabel!.text = object["title"]
         cell.detailTextLabel!.text = object["body"]
         return cell
     }
-
-
+    
+    
 }
 
